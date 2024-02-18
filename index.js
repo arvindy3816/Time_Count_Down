bd9fb33d8389f0f713f9f408274ae3260db4b907
Arvind yadav <arvindy3816n@gmail.com>
2024-02-18 12:26:11 +0530

    fourth commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const endDate = "17april 2024 4:10 pm";
+document.getElementById("end-Date").innerText = endDate;
+
+const inputs  = document.querySelectorAll("input");
+
+function clock(){
+    const end = new Date(endDate)
+    const now = new Date()
+    const diff = (end - now) / 1000;
+    // console.log(diff);
+    if(diff < 0) return;
+    // conver into days
+    inputs[0].value = Math.floor(diff / 3600 / 24); 
+    inputs[1].value = Math.floor(diff / 3600 % 24); 
+    inputs[2].value = Math.floor(diff / 60) % 60;
+    inputs[3].value = Math.floor(diff) % 60; 
+    // convert into days
+}
+// inital call
+clock()
+
+setInterval(
+    () =>{
+        clock()
+    },
+    1000
+)
